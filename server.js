b27e80732b63cb24bd629c580663d1b5d5896625
Martijn van Ginkel <martijnvanginkel2@gmail.com>
2020-07-18 18:48:48 +0200

    Made server listen to port 5000

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
-var proxyPort = 80;
-var http = require('http');
-var httpProxy = require('http-proxy');
+var express = require('express');
+var app = express();
 
-var options = {
-    router: {
-        'localhost': '127.0.0.1:3000',
-        'somesite.com': '127.0.0.1:3000',
-        'othersite.com': '127.0.0.1:4000'
-    }
-};
-console.log('Proxy Routing:');
-console.log(options);
-console.log();
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/public');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
-var proxyServer = http.createServer(options);
-proxyServer.listen(proxyPort);
-console.log('Proxy listening on port ' + proxyPort);
+app.get('/', function(req, res){
+    res.render('index.html');
+});
+
+app.listen(5000);
